f4db6acde5d8e00ac4c1b278d67865f661c2c388
Yury Gargay <yury.gargay@gmail.com>
2016-07-07 13:06:27 +0300

    Add MPL 2.0 and prepare to hexpm publish

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule MixDiaCompiler.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     description: description,
+     package: package]
   end
 
   def application do
@@ -16,5 +18,19 @@ defmodule MixDiaCompiler.Mixfile do
 
   defp deps do
     []
+  end
+
+  defp description do
+    """
+    Diameter source files compiler for Mix and Elixir.
+    """
+  end
+
+  defp package do
+    [name: :mix_dia_compiler,
+     files: ["lib", "mix.exs", "README.md", "LICENSE"],
+     maintainers: ["Yury Gargay"],
+     licenses: ["MPL 2.0"],
+     links: %{"GitHub" => "https://github.com/xerions/mix_dia_compiler"}]
   end
 end
